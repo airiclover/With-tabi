975433029475355386f1beab0eb8dd496a57a315
airi <airi-love-clover@rakuten.jp>
2021-07-05 21:34:34 +0900

    💄 Update UI.

--- src/components/Layout/PlanLayout.jsx
+++ src/components/Layout/PlanLayout.jsx
@@ -8,7 +8,7 @@ export const PlanLayout = (props) => {
       <div>
         <Header />
       </div>
-      <div className="min-h-screen px-3 bg-gray-100 text-gray-800">
+      <div className="min-h-screen pb-10 px-3 bg-gray-100 text-gray-800">
         {children}
       </div>
     </>
