94eda5ead9caf9c0df5f8709fb702d1d94a9d33a
airi <airi-love-clover@rakuten.jp>
2021-07-11 16:38:00 +0900

    🎨 Improve structure  of the code.

---
+++ src/components/common/assets/GoogleIcon.jsx
@@ -0,0 +1,19 @@
+export const GoogleIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-brand-google"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      strokeWidth="3"
+      stroke="#ffffff"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M17.788 5.108a9 9 0 1 0 3.212 6.892h-8" />
+    </svg>
+  );
+};
---
+++ src/components/common/assets/PlusIcon.jsx
@@ -0,0 +1,16 @@
+export const PlusIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-10 w-10 text-white absolute top-1.5 left-3"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
