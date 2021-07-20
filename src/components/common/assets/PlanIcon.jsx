3eef956f2aeebd34b44a9b025a0551ba257cd8a1
airi <airi-love-clover@rakuten.jp>
2021-07-20 22:43:28 +0900

    🔥 Remove text.

--- src/components/common/assets/PlanIcon.jsx
+++ src/components/common/assets/PlanIcon.jsx
@@ -2,7 +2,6 @@ export const PlanIcon = (props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      // className={`h-${props.size} w-${props.size}`}
       className={props.className}
       fill="none"
       viewBox="0 0 24 24"
