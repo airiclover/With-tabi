72eec70061dcb2ae439676c0a050e92c41d6eb6c
airi <airi-love-clover@rakuten.jp>
2021-07-25 15:55:25 +0900

    ✨ Add links.

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,7 @@
 import { Auth } from "src/components/common/Auth";
 
 const LogIn = () => {
-  return <Auth page="login" />;
+  return <Auth page="login" textlink="signup" text="初めて登録の方はこちら→" />;
 };
 
 export default LogIn;
