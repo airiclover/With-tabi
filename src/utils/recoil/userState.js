a2c470a64f16ba11c2c684e9cb5960e8b7d14c85
airi <airi-love-clover@rakuten.jp>
2021-07-27 21:04:27 +0900

    🎨 Improve structure recoil state.

--- src/utils/recoil/userState.js
+++ src/utils/recoil/userState.js
@@ -2,12 +2,5 @@ import { atom } from "recoil";
 
 export const userState = atom({
   key: "userState",
-  default: {
-    uid: "",
-    name: "",
-    icon: "",
-    twitter: "",
-    instagram: "",
-    introduce: "",
-  },
+  default: undefined,
 });
