e54caa3b5d9746e387eb3e31c9571199b5603c54
airi <airi-love-clover@rakuten.jp>
2021-08-02 01:17:05 +0900

    ♻️ Refactor code and add PlanForm file.

--- src/pages/[userId]/plan.jsx
+++ src/pages/[userId]/plan.jsx
@@ -1,49 +1,28 @@
 import Link from "next/link";
 import Image from "next/image";
-import firebase from "src/utils/firebase/firebase";
-import { useRouter } from "next/router";
-import { Fragment, useEffect, useState } from "react";
-import { Dialog, Transition } from "@headlessui/react";
+import { useEffect, useState } from "react";
 import { db } from "src/utils/firebase/firebase";
-import { EmojiMart } from "src/utils/emojimart";
-import { useForm } from "react-hook-form";
+import { PlanForm } from "src/components/plan/PlanForm";
 import { Emoji } from "emoji-mart";
 import { CommonLayout } from "src/components/Layout/CommonLayout";
 import { useCurrentUser } from "src/components/common/hooks/useCurrentUser";
 import { useRequireLogin } from "src/components/common/hooks/useRequireLogin";
 import { CalendarIcon } from "src/components/common/assets/CalendarIcon";
 import { PlusIcon } from "src/components/common/assets/PlusIcon";
-import { CloseIcon } from "src/components/common/assets/CloseIcon";
-import { EmojiIcon } from "src/components/common/assets/EmojiIcon";
 
 const UserPlanPage = () => {
   const [plans, setPlans] = useState([]);
   const [isOpenModal, setIsOpenModal] = useState(false);
-  const [isOpenEmoji, setIsOpenEmoji] = useState(false);
-  const [emoji, setEmoji] = useState(null); //アイコン
-  const [today, setToday] = useState("");
 
-  const router = useRouter();
   const { userInfo } = useCurrentUser();
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm();
 
   useRequireLogin();
 
-  // ---------------------------------------
   useEffect(() => {
     getUsersPlans();
-    getToday();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // ---------------------------------------
-
-  // ------------プラン取得チェック--------------
   const getUsersPlans = () => {
     console.log("プラン取得チェック");
     db.collection("plans")
@@ -77,20 +56,7 @@ const UserPlanPage = () => {
         console.log("プランデータ【エラー】だよ！", error);
       });
   };
-  // ---------------------------------------
 
-  // ------------今日の日付取得---------------
-  const getToday = () => {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = ("00" + (today.getMonth() + 1)).slice(-2);
-    const date = ("00" + today.getDate()).slice(-2);
-    const formatDate = year + month + date;
-    setToday(formatDate);
-  };
-  // ---------------------------------------
-
-  // ------------モーダル操作-----------------
   const openModal = () => {
     setIsOpenModal(true);
   };
@@ -98,171 +64,15 @@ const UserPlanPage = () => {
   const closeModal = () => {
     setIsOpenModal(false);
   };
-  // ---------------------------------------
-
-  // ------------絵文字マート-----------------。
-  const openEmoji = () => {
-    setIsOpenEmoji((isOpenEmoji) => !isOpenEmoji);
-  };
-  // ---------------------------------------
-
-  // ---------------フォーム-----------------
-  const on_submit = (data) => {
-    console.log("on_submit関数内！！！");
-
-    db.collection("plans")
-      .add({
-        userID: userInfo?.uid,
-        title: data.title,
-        planIcon: emoji,
-        startDate: data.startDate,
-        lastDate: data.lastDate,
-        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-        updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
-      })
-      .then(async () => {
-        const result = {
-          title: "",
-          startDate: "",
-          lastDate: "",
-        };
-        reset(result);
-
-        setEmoji(null);
-        closeModal();
-        getUsersPlans(); //startDateを降順でソートしたものを反映したいため関数呼び出し
-        await router.push(`/${userInfo.uid}/plan`);
-      })
-      .catch((error) => {
-        console.error("Error adding document: ", error);
-      });
-  };
-  // ---------------------------------------
 
   return (
     <CommonLayout>
-      <Transition appear show={isOpenModal} as={Fragment}>
-        <Dialog
-          as="div"
-          className="fixed inset-0 z-10 overflow-y-auto text-gray-800"
-          onClose={closeModal}
-        >
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            <div className="min-h-screen pt-6 px-4 inline-block w-full max-w-md overflow-hidden text-left align-middle transition-all transform bg-white">
-              <div className="text-right">
-                <button
-                  type="button"
-                  className="p-2 bg-gray-100 rounded-full hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-gray-500"
-                  onClick={closeModal}
-                >
-                  <CloseIcon />
-                </button>
-              </div>
-
-              <div className="pt-6 pb-7">
-                <p className="pb-1 font-semibold">アイコンを選択</p>
-                <div className="flex">
-                  <button
-                    onClick={openEmoji}
-                    className="p-2.5 bg-gray-100 rounded-lg mr-3"
-                  >
-                    <EmojiIcon className="w-6 h-6" />
-                  </button>
-                  <p className="pt-2">
-                    {emoji ? <Emoji emoji={emoji} size={30} /> : null}
-                  </p>
-                </div>
-
-                {/* emoji-mart */}
-                {isOpenEmoji ? (
-                  <EmojiMart
-                    setEmoji={setEmoji}
-                    setIsOpenEmoji={setIsOpenEmoji}
-                  />
-                ) : null}
-              </div>
-
-              <form onSubmit={handleSubmit(on_submit)}>
-                <label className="pb-7 font-semibold flex flex-col">
-                  旅行タイトル
-                  <input
-                    placeholder="旅行タイトル"
-                    {...register("title", {
-                      required: true,
-                      minLength: 1,
-                      maxLength: 50,
-                    })}
-                    className="w-full mt-1 p-2 bg-gray-100 rounded-lg"
-                  />
-                  {errors.title && (
-                    <span className="pt-1 text-red-500 text-xs">
-                      入力は必須です(50文字以内)
-                    </span>
-                  )}
-                </label>
-
-                <label className="pb-7 font-semibold flex flex-col">
-                  出発日
-                  <input
-                    type="number"
-                    placeholder={today}
-                    {...register("startDate", {
-                      required: true,
-                      pattern: /[0-9]{8}/,
-                      maxLength: 8,
-                    })}
-                    className="w-full mt-1 p-2 bg-gray-100 rounded-lg"
-                  />
-                  {errors.startDate && (
-                    <span className="pt-1 text-red-500 text-xs">
-                      入力は必須です(数値8文字)
-                    </span>
-                  )}
-                </label>
-
-                <label className="pb-12 font-semibold flex flex-col">
-                  帰着日
-                  <input
-                    type="number"
-                    placeholder={today}
-                    {...register("lastDate", {
-                      required: true,
-                      pattern: /[0-9]{8}/,
-                      maxLength: 8,
-                    })}
-                    className="w-full mt-1 p-2 bg-gray-100 rounded-lg"
-                  />
-                  {errors.lastDate && (
-                    <span className="pt-1 text-red-500 text-xs">
-                      入力は必須です(数値8文字)
-                    </span>
-                  )}
-                </label>
-
-                <div className="text-right">
-                  <button
-                    type="submit"
-                    className="px-8 py-3 bg-yellow-500 text-white tracking-widest rounded-full hover:opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-                  >
-                    登録
-                  </button>
-                </div>
-              </form>
-            </div>
-          </Transition.Child>
-        </Dialog>
-      </Transition>
-
-      {/* ========================== */}
-      {console.log("plansコンポーネント内")}
+      <PlanForm
+        userInfo={userInfo}
+        isOpenModal={isOpenModal}
+        closeModal={closeModal}
+        getUsersPlans={getUsersPlans}
+      />
 
       <div className="px-3 pb-28">
         <div className="py-4 flex items-center relative">
