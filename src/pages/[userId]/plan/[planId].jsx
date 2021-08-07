5fe58dc262db7eccbfe347b442169648bfab9c73
airi <airi-love-clover@rakuten.jp>
2021-08-07 19:05:55 +0900

    💄 Update the UI.

--- src/pages/[userId]/plan/[planId].jsx
+++ src/pages/[userId]/plan/[planId].jsx
@@ -1,9 +1,73 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { useRequireLogin } from "src/components/common/hooks/useRequireLogin";
+import { db } from "src/utils/firebase/firebase";
+import { Emoji } from "emoji-mart";
+import { CommonLayout } from "src/components/layouts/CommonLayout";
+
 const PlanId = () => {
+  const [plan, setPlan] = useState();
+  const [startDate, setStartDate] = useState();
+  const [lastDate, setLastDate] = useState();
+  const router = useRouter();
+  console.log(router.query.planId);
+
+  useRequireLogin();
+
+  useEffect(() => {
+    getPlan();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getPlan = async () => {
+    const planDoc = db.collection("plans").doc(router.query.planId);
+
+    await planDoc
+      .get()
+      .then((doc) => {
+        const docData = doc.data();
+
+        const fixDate = (data) => {
+          const date = data.replace(/-/g, "/");
+          const setDate = new Date(date);
+          const arrayWeek = ["日", "月", "火", "水", "木", "金", "土"];
+          const week = `(${arrayWeek[setDate.getDay()]})`;
+
+          return date + week;
+        };
+
+        const fixStartDate = fixDate(docData.startDate);
+        const fixLastDate = fixDate(docData.lastDate);
+
+        if (doc.exists) {
+          setPlan(docData);
+          setStartDate(fixStartDate);
+          setLastDate(fixLastDate);
+        } else {
+          console.log("データないよ！！！");
+        }
+      })
+      .catch((error) => {
+        // toast.dismiss();
+        console.log("planDetailページエラーだよ！:", error);
+        // toast.error("エラーが発生しました。時間をおいてから試してください。");
+      });
+  };
+
   return (
-    <div>
-      <h1>PlanDetail</h1>
-      <div>このページにプラン詳細を表示させる</div>
-    </div>
+    <CommonLayout>
+      {plan ? (
+        <div className="py-6 px-4  font-extrabold">
+          <h1 className="text-2xl">
+            <Emoji emoji={plan?.planIcon} size={25} />
+            {plan?.title}
+          </h1>
+          <p className="py-2 text-right">{`${startDate} - ${lastDate}`}</p>
+        </div>
+      ) : (
+        <div>ローディング中</div>
+      )}
+    </CommonLayout>
   );
 };
 
