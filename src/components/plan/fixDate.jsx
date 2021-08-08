38452f68542d0333e156f5393d880c0c74f2b843
airi <airi-love-clover@rakuten.jp>
2021-08-08 22:10:45 +0900

    🚚 Rename file and remove log.

--- src/components/plan/fixDate.jsx
+++ src/components/plan/fixDate.jsx
@@ -1,6 +1,5 @@
-export const fixDate = () => {
+export const FixDate = () => {
   const fixedDate = (data) => {
-    console.log("fixxxxxxxxx");
     const date = data.replace(/-/g, "/");
     const setDate = new Date(date);
     const arrayWeek = ["日", "月", "火", "水", "木", "金", "土"];
