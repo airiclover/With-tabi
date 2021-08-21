f7425dfbf414c23a9f6b448f17841a8b572e11d9
airi <airi-love-clover@rakuten.jp>
2021-08-21 19:28:16 +0900

    🔧 Add firebase storage domain.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   images: {
-    domains: ["pbs.twimg.com", "lh3.googleusercontent.com"],
+    domains: [
+      "pbs.twimg.com",
+      "lh3.googleusercontent.com",
+      "firebasestorage.googleapis.com",
+    ],
   },
 };
