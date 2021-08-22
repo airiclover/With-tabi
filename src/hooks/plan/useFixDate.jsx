dad101d8e8ff458a1aa4106f518b4f015424e7e6
airi <airi-love-clover@rakuten.jp>
2021-08-23 01:54:41 +0900

    ✨ Add customHooks.

---
+++ src/hooks/plan/useFixDate.jsx
@@ -0,0 +1,14 @@
+export const useFixDate = () => {
+  const fixedDate = (data) => {
+    const date = data.replaceAll("-", "/");
+    const setDate = new Date(date);
+    const arrayWeek = ["日", "月", "火", "水", "木", "金", "土"];
+    const week = `(${arrayWeek[setDate.getDay()]})`;
+
+    return date + week;
+  };
+
+  return {
+    fixedDate,
+  };
+};
