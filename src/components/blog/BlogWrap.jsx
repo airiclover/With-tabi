8289473041f1dac9f57024558c2c21476abb3eef
airi <airi-love-clover@rakuten.jp>
2021-09-22 22:55:07 +0900

    🔥 Remove code.

--- src/components/blog/BlogWrap.jsx
+++ src/components/blog/BlogWrap.jsx
@@ -57,25 +57,6 @@ export const BlogWrap = (props) => {
               />
               <div className="pl-1 text-xs">{props?.account.name}</div>
             </div>
-
-            {/* 👇 お気に入り機能つける？ */}
-            {/* <div className="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-red-500"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-              </svg>
-              <div className="pt-0.5 text-xs">500</div>
-            </div> */}
           </a>
         </Link>
       </div>
